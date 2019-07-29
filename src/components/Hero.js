fd4da8bfff10d2bf7b9d6aff67ce6504264e9656
nikolaounkl <nikolaounkl@gmail.com>
2019-07-29 22:06:54 +0200

    Updated navbar

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,27 +1,26 @@
-import React, {useContext} from 'react';
-import HeroNavbar from './HeroNavbar';
-import {ThemeContext} from '../contexts/ThemeContext';
+import React, { useContext } from "react";
+import HeroNavbar from "./HeroNavbar";
+import { ThemeContext } from "../contexts/ThemeContext";
 
 const Hero = () => {
-    const {darkMode} = useContext(ThemeContext);
-    const theme = darkMode ? "hero is-dark is-fullheight" : "hero is-light is-fullheight";
+    const { darkMode } = useContext(ThemeContext);
+    const theme = darkMode
+        ? "hero is-black is-fullheight"
+        : "hero is-white is-fullheight";
     return (
         <section className={theme}>
             <HeroNavbar />
             <div className="hero-body">
                 <div className="container has-text-centered">
-                    <i className="fas fa-user-astronaut fa-4x"></i>
-                    <br/><br/>
-                    <h1 className="title">
-                        Welcome
-                    </h1>
-                    <h2 className="subtitle">
-                        to my react planet
-                    </h2>
+                    <i className="fas fa-user-astronaut fa-4x" />
+                    <br />
+                    <br />
+                    <h1 className="title">Welcome</h1>
+                    <h2 className="subtitle">to my react planet</h2>
                 </div>
             </div>
         </section>
-    )
-}
+    );
+};
 
-export default Hero
+export default Hero;
--- src/components/HeroNavbar.js
+++ src/components/HeroNavbar.js
@@ -1,31 +1,29 @@
-import React, {useContext} from 'react';
-import {ThemeContext} from '../contexts/ThemeContext';
+import React, { useContext } from "react";
+import { ThemeContext } from "../contexts/ThemeContext";
 
 const HeroNavbar = () => {
-    const {darkMode, toggleTheme} = useContext(ThemeContext);
-    const icon = darkMode ? "fas fa-sun fa-lg" : "fas fa-moon fa-lg";
+    const { darkMode, toggleTheme } = useContext(ThemeContext);
+    const icon = darkMode
+        ? "fas fa-radiation fa-lg has-text-warning"
+        : "fas fa-radiation fa-lg has-text-dark";
     return (
         <div className="hero-head">
-            <header className="navbar">
-                <div className="container">
+            <div className="container">
+                <header className="navbar">
                     <div className="navbar-brand">
-                        <span className="navbar-item"><h1 className="is-size-4">NKL</h1></span>
+                        <span className="navbar-item">
+                            <h1 className="is-size-4">NKL</h1>
+                        </span>
                     </div>
-                    <div id="navbarMenuHeroC" className="navbar-menu">
-                        <div className="navbar-end">
-                            <span className="navbar-item">
-                                <button onClick={toggleTheme} className="button is-dark" href="#" rel="noopener noreferrer">
-                                    <span className="icon has-text-warning">
-                                        <i className={icon}></i>
-                                    </span>
-                                </button>
-                            </span>
-                        </div>
+                    <div className="navbar-item">
+                        <span className="icon" onClick={toggleTheme}>
+                            <i className={icon} />
+                        </span>
                     </div>
-                </div>
-            </header>
+                </header>
+            </div>
         </div>
-    )
-}
+    );
+};
 
-export default HeroNavbar
+export default HeroNavbar;
